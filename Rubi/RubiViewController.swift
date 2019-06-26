d3557a7bd8c68f8d335912da8017569b17c538a7
佐川晴海 <sagawaharumi@sagawaharuminoMacBook-puro.local>
2019-06-26 19:40:01 +0900

    feat: Change rootViewController

---
+++ Rubi/RubiViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RubiViewController.swift
+//  Rubi
+//
+//  Created by 佐川晴海 on 2019/06/26.
+//  Copyright © 2019 佐川晴海. All rights reserved.
+//
+
+import UIKit
+
+class RubiViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor(red: 247/255, green: 247/255, blue: 247/255, alpha: 1)
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
