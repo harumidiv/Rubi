4dd13414813066dce015bd2eeb725c0479923a8d
佐川晴海 <sagawaharumi@sagawaharuminoMacBook-puro.local>
2019-06-26 19:34:55 +0900

    feat: Create a project

---
+++ Rubi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rubi
+//
+//  Created by 佐川晴海 on 2019/06/26.
+//  Copyright © 2019 佐川晴海. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
