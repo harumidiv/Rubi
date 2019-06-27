801ff22e47ddc6b0afe66b0addd2e7da1c23ba34
佐川晴海 <sagawaharumi@sagawaharuminoMacBook-puro.local>
2019-06-27 23:19:49 +0900

    feat: Add tableView Extension

---
+++ Rubi/View/RubiTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RubiTableViewCell.swift
+//  Rubi
+//
+//  Created by 佐川晴海 on 2019/06/27.
+//  Copyright © 2019 佐川晴海. All rights reserved.
+//
+
+import UIKit
+
+class RubiTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
