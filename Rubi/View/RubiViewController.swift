be044e99b6a7ba3418cfb1724171c446fe62900e
佐川晴海 <sagawaharumi@sagawaharuminoMacBook-puro.local>
2019-06-27 21:43:18 +0900

    feat: Add reflect text in UI

--- Rubi/View/RubiViewController.swift
+++ Rubi/View/RubiViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class RubiViewController: UIViewController {
+    @IBOutlet weak var rubiTextField: UITextField! {
+        didSet {
+            rubiTextField.delegate = self
+        }
+    }
     
+    @IBOutlet weak var rubiLabel: UILabel!
     lazy var presenter: RubiPresenter =  {
         return RubiPresenterImpl(model: RubiModelImpl(), output: self)
     }()
@@ -17,12 +23,28 @@ class RubiViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor(red: 247/255, green: 247/255, blue: 247/255, alpha: 1)
-        presenter.requestAPI(text: "百鬼夜行")
     }
 }
 
 extension RubiViewController: RubiPresenterOutput {
     func convertText(hiragana: String) {
-        print(hiragana)
+        DispatchQueue.main.async {
+            self.rubiLabel.text = hiragana
+        }
+    }
+}
+
+extension RubiViewController: UITextFieldDelegate{
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        guard let text = textField.text, text.count > 0 else {
+            print("からです")
+            rubiLabel.text = "文字を入力してください"
+            return true
+        }
+        
+        presenter.requestAPI(text: text)
+        textField.resignFirstResponder()
+        return true
     }
 }
