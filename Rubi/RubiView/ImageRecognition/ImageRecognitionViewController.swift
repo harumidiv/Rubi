3104b9cd27ce099c54112d8d9fc26053a8aa8fd7
harumidiv <harumidiv@gmail.com>
2019-10-26 17:59:08 +0900

    feat: フォトライブラリへのアクセスを追加しました

--- Rubi/RubiView/ImageRecognition/ImageRecognitionViewController.swift
+++ Rubi/RubiView/ImageRecognition/ImageRecognitionViewController.swift
@@ -11,6 +11,22 @@ import UIKit
 
 class ImageRecognitionViewController: UIViewController {
 
+    
+    @IBOutlet weak var userPhoto: UIImageView! {
+        didSet {
+            picker.sourceType = UIImagePickerController.SourceType.photoLibrary
+
+            //デリゲートを設定する
+            picker.delegate = self
+
+            //現れるピッカーNavigationBarの文字色を設定する
+            picker.navigationBar.tintColor = UIColor.white
+
+            //現れるピッカーNavigationBarの背景色を設定する
+            picker.navigationBar.barTintColor = UIColor.gray
+        }
+    }
+    var picker: UIImagePickerController! = UIImagePickerController()
 
     private lazy var presentor: ImageRecognitionPresentor = {
         return ImageRecognitionPresentorImpl(model: ImageRecognitionModelImpl(), output: self)
@@ -18,20 +34,40 @@ class ImageRecognitionViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .green
 
 
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard let image = UIImage(named: "sample.jpeg") else { return }
-                presentor.recognition(image: image)
+//        guard let image = UIImage(named: "sample.jpeg") else { return }
+//        presentor.recognition(image: image)
+        
+        //ピッカーを表示する
+        present(picker, animated: true, completion: nil)
     }
 }
+// MARK: - Extension ImageRecognitionPresentorOutput
 
 extension ImageRecognitionViewController: ImageRecognitionPresentorOutput {
     func showRecognitionMessage(message: String) {
         self.view.backgroundColor = .white
         print(message)
+    }
+}
+
+// MARK: - Extension 
+
+extension ImageRecognitionViewController: UIImagePickerControllerDelegate,UINavigationControllerDelegate{
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]){
+
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+           
+            userPhoto.image = image
+        } else{
+            print("Error")
+        }
+
+        self.dismiss(animated: true, completion: nil)
     }
 }
