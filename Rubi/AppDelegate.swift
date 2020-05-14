258261f972fb234cffaff0d94641cfde3db2d63e
佐川晴海[YUMEMI] / Sagawa Harumi <h_sagawa@yumemi.co.jp>
2020-05-14 17:10:44 +0900

    feat: TabBarの色を修正しました

--- Rubi/AppDelegate.swift
+++ Rubi/AppDelegate.swift
@@ -17,9 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         UserStore.isHiragana = true
-
         window?.rootViewController = TabController()
         UINavigationBar.appearance().barTintColor = UIColor(red: 229/255, green: 114/255, blue: 31/255, alpha: 1.0)
+        UITabBar.appearance().tintColor = UIColor(red: 229/255, green: 114/255, blue: 31/255, alpha: 1.0)
         UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor : UIColor.white]
         
         return true
